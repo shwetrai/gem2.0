f84384b735fcfb04c5bcb1c7d05b34c1a171e659
SHWETKETU RAI <shwetrai@in.ibm.com>
2025-06-24 17:29:14 +0530

    updated flow

--- src/main/java/tcspoc/gem/services/ApplicantService.java
+++ src/main/java/tcspoc/gem/services/ApplicantService.java
@@ -37,28 +37,32 @@ public class ApplicantService {
     public Applicant updateApplicantName(Applicant applicant) {
         String arn = applicant.getArn();
         UpdateName update = new UpdateName();
-        update.setName(applicant.getName());
+        //update.setName(applicant.getName());
+        update.setName("bob");
         return applicationRemoteService.updateName(arn, update);
     }
 
     public Applicant updateApplicantEmail(Applicant applicant) {
         String arn = applicant.getArn();
         UpdateEmail update = new UpdateEmail();
-        update.setEmail(applicant.getEmail());
+       // update.setEmail(applicant.getEmail());
+       update.setEmail("bob@abc.com");
         return applicationRemoteService.updateEmail(arn, update);
     }
 
     public Applicant updateApplicantBank(Applicant applicant) {
         String arn = applicant.getArn();
         UpdateBank update = new UpdateBank();
-        update.setBank_name(applicant.getBank_name());
+        // update.setBank_name(applicant.getBank_name());
+        update.setBank_name("Focus Bank");
         return applicationRemoteService.updateBank(arn, update);
     }
 
     public Applicant updateApplicantAccount(Applicant applicant) {
         String arn = applicant.getArn();
         UpdateAccountNo update = new UpdateAccountNo();
-        update.setAccount_no(applicant.getAccount_no());
+        // update.setAccount_no(applicant.getAccount_no());
+         update.setAccount_no("1234567");
         return applicationRemoteService.updateAccount(arn, update);
     }
 
